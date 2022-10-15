1a6121d68667a1e1329c40f12eff14a87b5f784a
gerardasb <gerardas.barstys@gmail.com>
2022-10-15 11:17:42 +0300

    first commit

---
+++ resources/views/listing.blade.php
@@ -0,0 +1,2 @@
+  <h2>{{$listing['headline']}}</h2>
+  <p>{{$listing['description']}}</p>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// All Listing
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
