a2aeff804218490d6fb7774b836cc3ebf3de7f74
gerardasb <gerardas.barstys@gmail.com>
2022-10-17 16:04:55 +0300

    commit: Delete feature added

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -27,7 +27,14 @@
             <i class="fa-solid fa-pencil"></i> Edit
         </a>
 
-    </x-card>
+        <form method="POST" action="/listings/{{$listing->id}}">
+        @csrf
+        @method('DELETE')
+        <button class="text-red-500"><i class="fa-solid fa-trash"></i>
+        Delete</button>
+        </form>
 
+
+    </x-card>
 </div>
 </x-layout>
