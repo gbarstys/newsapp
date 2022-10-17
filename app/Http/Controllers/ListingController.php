8d7e862784b16304eaa79329d8363da2f92695b2
gerardasb <gerardas.barstys@gmail.com>
2022-10-17 10:44:48 +0300

    commit: search functionality added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,7 +10,7 @@ class ListingController extends Controller
     // Show all listings
     public function index() {
         return view('listings.index', [
-            'listings' => Listing::latest()->get()
+            'listings' => Listing::latest()->filter(request(['search']))->get()
         ]);
     }
 
