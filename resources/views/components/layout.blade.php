61d315cdc7c0449a58d7ef2e699d7eb8b0efc1a7
gerardasb <gerardas.barstys@gmail.com>
2022-10-17 12:26:14 +0300

    commit: flash messages added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        <script src="//unpkg.com/alpinejs" defer></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
             tailwind.config = {
@@ -60,5 +61,7 @@
                 >Post News</a
             >
         </footer>
+
+        <x-flash-message />
     </body>
 </html>
